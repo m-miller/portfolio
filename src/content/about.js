c79e618f711ce15d7de6a544656601823a7b25b0
Martin Miller <milleronic@gmail.com>
2024-10-12 12:09:09 -0500

    rm unused assets

--- src/content/about.js
+++ src/content/about.js
@@ -3,14 +3,14 @@ export const about = [
       title: "Lead Web Developer",
       location: "Merchology",
       duration: "March 2024 - September 2024",
-      icon: "/assets/about/experience.png",
+      icon: "/assets/projects/merchology.png",
       link: "https://m-miller.github.io/resume/#march-2024---september-2024--lead-web-developer--merchology-plymouth-minnesota"
     },
     {
       title: "Senior Web Developer",
       location: "Merchology",
       duration: "April 2022 - March 2024",
-      icon: "/assets/about/experience.png",
+      icon: "/assets/projects/merchology.png",
       link: "https://m-miller.github.io/resume/#april-2022---march-2024--senior-web-developer--merchology-plymouth-minnesota"
     },
   ];
