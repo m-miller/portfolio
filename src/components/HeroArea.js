91d11d8496f3bba2757817ca0d9c3ccb7e47a1e2
Martin Miller <milleronic@gmail.com>
2024-10-13 14:11:43 -0500

    update headline

--- src/components/HeroArea.js
+++ src/components/HeroArea.js
@@ -6,7 +6,7 @@ export const HeroArea = () => {
       <img className={styles.profile} src={`${process.env.PUBLIC_URL}/assets/me.png`} alt="profile" />
       <h1 className="title">Hi, I'm Martin!</h1>
       <p className="description">
-        An experienced Full Stack Web Developer looking for a new role.
+        An experienced MERN/LAMP Full Stack Web Developer looking for a new role.
       </p>
       <div className={styles.buttonBox}>
         <button
